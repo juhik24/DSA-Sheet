763a18eb48160d83e172681391d02eea6a2d8153
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-08 23:36:20 +0530

    Time: 532 ms (8.69%), Space: 120.2 MB (11.18%) - LeetHub

---
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int n = candyType.size();
+        map<int, int>mp;
+        //mp.insert({candyType[0], 1});
+        map<int, int>::iterator it;
+        for(int i = 0; i < n; i++) {
+            it = mp.find(candyType[i]);
+            if(it == mp.end()) {
+                mp.insert(make_pair(candyType[i], 1));
+            }
+            else {
+                it->second++;
+            }
+        }
+        int m = mp.size();
+        int cnt = n/2;
+        if(m > cnt) return cnt;
+        else return m;
+    }
+};
