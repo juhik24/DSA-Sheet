4068321b9531c282359a2d27c6a0960f06b9393d
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-13 02:38:21 +0530

    Time: 378 ms (66.04%), Space: 8 MB (74.86%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int di[4] = {-1, 0, 1, 0};
+    int dj[4] = {0, -1, 0, 1};
+    
+    bool explore(vector<vector<char>>&board, int i, int j, int size, string &word) {
+        if(size == word.length()) return true;
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) return false;
+        if(word[size] != board[i][j]) return false;
+        char ch = board[i][j];
+        board[i][j] = '@';
+        for(int k = 0; k < 4; k++) {
+            bool ans = explore(board, i+di[k], j+dj[k], size+1, word);
+            if(ans) {
+                board[i][j] = ch;
+                return true;
+            }
+        }
+        board[i][j] = ch;
+        return false;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(board[i][j] != word[0]) continue;
+                bool ans = explore(board, i, j, 0, word);
+                if(ans) return ans;
+            }
+        }
+        return false;
+    }
+};
