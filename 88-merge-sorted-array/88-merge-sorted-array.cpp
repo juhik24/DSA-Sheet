019c630d4c662781dbb89f9847e202a72341f12b
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-14 00:42:37 +0530

    Time: 6 ms (26.52%), Space: 9.1 MB (31.73%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int ans[m+n];
+        vector<int>ans(m+n);
         int i = 0, j = 0, k = 0;
         while(i < m && j < n) {
             if(nums1[i] < nums2[j]) {
