4e6ee75607f8808409be88ac369b78d0b8c4a84d
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-15 22:53:05 +0530

    Time: 3 ms (43.51%), Space: 6.9 MB (85.71%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int di[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+    int dj[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+    
+    int explore(vector<vector<int>>&board, int i, int j, int n, int m) {
+        int cnt=0;
+        for(int k = 0; k < 8; k++) {
+            if(i+di[k] < 0 || i+di[k] >= n || j+dj[k] < 0 || j+dj[k] >= m) continue;
+            else if(board[i+di[k]][j+dj[k]] == 1) cnt++;
+        }
+        //cout << cnt << " ";
+        return cnt;
+    }
+    
+    void gameOfLife(vector<vector<int>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>>ans(n);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                ans[i].push_back(board[i][j]);
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                int cnt;
+                cnt = explore(board, i, j, n, m);
+                if((cnt < 2 || cnt > 3) && board[i][j] == 1) ans[i][j] = 0;
+                else if((cnt == 2 || cnt == 3) && board[i][j] == 1) continue;
+                else if(cnt == 3 && board[i][j] == 0) ans[i][j] = 1;
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                board[i][j] = ans[i][j];
+            }
+        }
+    }
+};
