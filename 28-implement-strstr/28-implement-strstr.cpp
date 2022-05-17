a68ca83e4d0cecbc6a6f610d76c5fae26eca8e44
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-17 16:09:17 +0530

    Time: 4 ms (47.71%), Space: 6.3 MB (44.00%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    bool explore(string haystack, string needle, int i, int size) {
-        int n = haystack.length();
-        while(i < n && size < needle.length()) {
-            if(haystack[i] != needle[size]) return false;
-            else if(haystack[i] == needle[size]) {
-                if(size == needle.length()-1) return true;;
-                size++;
-                i++;
-            }
-        }
-        if(size-1 == needle.length()-1) return true;
-        else return false;
-    }
-    
     int strStr(string haystack, string needle) {
         int n = haystack.length();
         int m = needle.length();
-        if(n < m) return -1;
-        if(m == 0 || n == 0) return 0;
-        bool ans;
+        if(m == 0) return 0;
+        int j = 0;
         for(int i = 0; i < n; i++) {
-            if(haystack[i] == needle[0]) {
-                ans = explore(haystack, needle, i, 0);
+            if(haystack[i] == needle[j]) {
+                for(j = 1; j < m; j++) {
+                    if(haystack[i+j] != needle[j]) break;
+                }
+                if(j == m) return i;
+                j=0;
             }
-            if(ans == true) return i;
         }
         return -1;
     }
