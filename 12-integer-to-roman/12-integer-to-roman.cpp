76be8313bd66972fd7fecbc1dcd9b6191561ce32
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-18 01:18:44 +0530

    Time: 14 ms (41.09%), Space: 5.9 MB (79.35%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int get_count(int n) {
+        int cnt = 0;
+        while(n != 0) {
+            n = n/10;
+            cnt++;
+        }
+        return cnt;
+    }
+    
+    string intToRoman(int num) {
+        string ans = "";
+        int n = 13;
+        int arr[13] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        string ch[13] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        for(int i = n-1; i >= 0; i--) {
+            if(num == arr[i]) {
+                ans+= ch[i];
+                break;
+            }
+            if(num/arr[i]>0) {
+                int cnt = num/arr[i];
+                while(cnt--) {
+                    ans += ch[i];
+                }
+            }
+            num = num%arr[i];
+        }
+        return ans;
+    }
+};
