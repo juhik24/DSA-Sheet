b85fe53c5ba710db255cafc8a1b686d11807aa26
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-25 02:29:39 +0530

    Time: 3 ms (70.78%), Space: 7 MB (11.04%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,83 +1,27 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string ans = "";
         int n = a.length();
         int m = b.length();
+        int carry = 0;
+        string ans = "";
         int i = n-1, j = m-1;
-        char extra='0';
-        while(i >= 0 && j >= 0) {
-            if(a[i] == '0' && b[j] == '0') {
-                if(extra == '1') {
-                    ans += '1';
-                    extra = '0';
-                }
-                else ans += '0';
+        while(i >= 0 || j >= 0 || carry != 0) {
+            int x = 0, y = 0;
+            if(i >= 0 && a[i] == '1') {
+                x = 1;
             }
-            else if(a[i] == '1' && b[j] == '0') {
-                if(extra == '1') ans += '0';
-                else ans += '1';
-            }
-            else if(a[i] == '0' && b[j] == '1') {
-                if(extra == '1') ans += '0';
-                else ans += '1';
-            }
-            else if(a[i] == '1' && b[j] == '1') {
-                if(extra == '1') {
-                    ans += '1';
-                }
-                else {
-                    ans += '0';
-                    extra = '1';
-                }
-            }
-            i--;
-            j--;
+            if(j >= 0  && b[j] == '1') y = 1;
+            int sum = (x+y+carry)%2;
+            ans = to_string(sum) + ans;
+            carry = (x+y+carry)/2;   
+            if(i >= 0) i--;
+            if(j >= 0) j--;
         }
-        while(i >= 0) {
-            if(a[i] == '0') {
-                if(extra == '1') {
-                    ans += '1';
-                    extra = '0';
-                }
-                else {
-                    ans += '0';
-                }
-            }
-            else if(a[i] == '1') {
-                if(extra == '1') {
-                    ans += '0';
-                }
-                else {
-                    ans += '1';
-                }
-            }
-            i--;
-        }
-        while(j >= 0) {
-            if(b[j] == '0') {
-                if(extra == '1') {
-                    ans += '1';
-                    extra = '0';
-                }
-                else {
-                    ans += '0';
-                }
-            }
-            else if(b[j] == '1') {
-                if(extra == '1') {
-                    ans += '0';
-                }
-                else {
-                    ans += '1';
-                }
-            }
-            j--;
-        }
-        if(extra == '1') {
-            ans += '1';
-        }
-        reverse(ans.begin(), ans.end());
+        // if(carry == 1) {
+        //     ans += '1';
+        // }
+        // reverse(ans.begin(), ans.end());
         return ans;
     }
 };
