6a964a559d10c0429703a40d774376da999bbdea
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-26 23:09:52 +0530

    Time: 23 ms (80.30%), Space: 11.3 MB (62.29%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -6,7 +6,7 @@ public:
         unordered_map<double, int>mp;
         float slope;
         int max = 0;
-        for(int i = 0; i < n-max-1; i++) {
+        for(int i = 0; i < n; i++) {
             int imax=0;
             for(int j = i+1; j < n; j++) {
                 if(points[j][0]-points[i][0] == 0) slope = INT_MAX;
