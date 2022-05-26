4d85fb4adb47a0a61d4c867c1f56670714300b29
Juhi <72224773+juhik24@users.noreply.github.com>
2022-05-26 23:03:22 +0530

    Time: 23 ms (80.30%), Space: 11.3 MB (60.60%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        if(n == 1 || n == 2) return n;
+        unordered_map<double, int>mp;
+        float slope;
+        int max = 0;
+        for(int i = 0; i < n-max-1; i++) {
+            int imax=0;
+            for(int j = i+1; j < n; j++) {
+                if(points[j][0]-points[i][0] == 0) slope = INT_MAX;
+                else {
+                    slope = (float)(points[j][1]-points[i][1])/(float)(points[j][0]-points[i][0]);
+                }
+                mp[slope]++;
+                if(mp[slope] > imax) imax = mp[slope];
+            }
+            mp.clear();
+            if(imax > max) max = imax;
+        }
+        return max+1;
+    }
+};
