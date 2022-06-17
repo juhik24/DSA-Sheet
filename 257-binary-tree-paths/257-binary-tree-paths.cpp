a28f02294a4900304d75859a3e411c809109808b
Juhi <72224773+juhik24@users.noreply.github.com>
2022-06-17 23:14:49 +0530

    Time: 9 ms (27.19%), Space: 13.7 MB (22.91%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void paths(TreeNode* root, vector<string>&ans, string &str) {
+        if(root->left == NULL && root->right == NULL) {
+            string temp = str;
+           // temp =  temp + "->" + ch;
+            temp = temp + "->" + to_string(root->val);
+            ans.push_back(temp);
+            return;
+        }
+        
+        string ch = to_string(root->val);
+        string temp = str;
+        temp =  temp + "->" + ch;
+        if(root->left) paths(root->left, ans, temp);
+        if(root->right) paths(root->right, ans, temp);
+    }
+    
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string>ans;
+        string str = "";
+        if(root == NULL) {
+            ans.push_back(str);
+            return ans;
+        }
+        string s = to_string(root->val);
+        str += s;
+        if(root->left == NULL && root->right == NULL) {
+            ans.push_back(str);
+            return ans;
+        }
+        if(root->left) paths(root->left, ans, str);
+        if(root->right) paths(root->right, ans, str);
+        return ans;
+    }
+};
