148d65bd119cad800764704ead2f4914e985093b
Juhi <72224773+juhik24@users.noreply.github.com>
2022-07-20 01:31:03 +0530

    Time: 43 ms (97.65%), Space: 17.1 MB (41.49%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int parent[20005];
+    int rank[20005];
+    
+    void makeSet() {
+        for(int i=1; i <= 20004; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+    
+    int findPar(int node) {
+        if(node == parent[node]) return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    
+    void unionSet(int a, int b) {
+        a = findPar(a);
+        b = findPar(b);
+        if(rank[a] < rank[b]) {
+            parent[a] = b;
+        }
+        else if(rank[b] < rank[a]) {
+            parent[b] = a; 
+        }
+        else {
+            parent[b] = a;
+            a++;
+        }
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        makeSet();
+        for(auto i : stones) {
+            unionSet(i[0], i[1]+10001);
+        }
+        unordered_set<int>s;
+        for(auto i : stones) {
+            int a = i[0];
+            s.insert(findPar(a));
+        }
+        int cnt = s.size();
+        return stones.size()-cnt;
+    }
+};
