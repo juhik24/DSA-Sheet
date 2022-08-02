8086c20f89f0bf8c0ea626862b4e29d5efd5933a
Juhi <72224773+juhik24@users.noreply.github.com>
2022-08-03 00:06:12 +0530

    Time: 7 ms (72.11%), Space: 7.7 MB (97.55%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int solve(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        if(n%2 == 0) return solve(n/2);
-        else return 1 + solve(n/2);
-    }
-    
     vector<int> countBits(int n) {
-        vector<int>ans(n+1);
-        for(int i = 0; i <= n; i++) {
-            ans[i] = solve(i);
+        vector<int>v(n+1);
+        v[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i%2 == 0) {
+                int a = i/2;
+                v[i] = v[a];
+            }
+            else {
+                int a = i/2;
+                v[i] = 1+v[a];
+            }
         }
-        return ans;
+        return v;
     }
 };
