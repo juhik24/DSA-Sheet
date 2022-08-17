3b8688aa2226273534b32948ab72cd974d7375df
Juhi <72224773+juhik24@users.noreply.github.com>
2022-08-18 00:36:31 +0530

    Time: 8 ms (34.91%), Space: 8.6 MB (61.19%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        // unordered_map<char, string>mp;
-        // mp.insert({'a', ".-"}); mp.insert({'b', "-..."}); mp.insert({'c', "-.-."}); mp.insert({'d', "-.."});
-        // mp.insert({'e', "."}); mp.insert({'f', "..-."}); mp.insert({'g', "--."}); mp.insert({'h', "...."});
-        // mp.insert({'i', ".."}); mp.insert({'j', ".---"}); mp.insert({'k', "-.-"}); mp.insert({'l', ".-.."});
-        // mp.insert({'m', "--"}); mp.insert({'n', "-."}); mp.insert({'o', "---"}); mp.insert({'p', ".--."});
-        // mp.insert({'q', "--.-"}); mp.insert({'r', ".-."}); mp.insert({'s', "..."}); mp.insert({'t', "-"});
-        // mp.insert({'u', "..-"}); mp.insert({'v', "...-"}); mp.insert({'w', ".--"}); mp.insert({'x', "-..-"});
         vector<string>morse = {".-","-...","-.-.","-..",".","..-.","--.",
                          "....","..",".---","-.-",".-..","--","-.",
                          "---",".--.","--.-",".-.","...","-","..-",
                          "...-",".--","-..-","-.--","--.."};
-        //mp.insert({'y', "-..-"}); mp.insert({'z', "--.."});
         unordered_set<string>s;
         for(int i = 0; i < words.size(); i++) {
             string str = "";
@@ -20,9 +12,6 @@ public:
                 str += morse[words[i][j]-'a'];
             }
             s.insert(str);
-        }
-        for(auto i : s) {
-         cout << i << ", ";
         }
         cout << endl;
         return s.size();
