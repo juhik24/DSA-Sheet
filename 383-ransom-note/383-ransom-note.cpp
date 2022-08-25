f18ded154d72fc0b046b6c535f0c6c5f48ff6e47
Juhi <72224773+juhik24@users.noreply.github.com>
2022-08-25 17:12:52 +0530

    Time: 33 ms (37.61%), Space: 8.9 MB (15.41%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int>mp;
+        int n = magazine.length();
+        for(int i = 0; i < n; i++) {
+            mp[magazine[i]]++;
+        }
+        int m = ransomNote.length();
+        for(int i = 0; i < m; i++) {
+            if(mp.find(ransomNote[i]) == mp.end() || mp[ransomNote[i]] == 0) return false;
+            mp[ransomNote[i]]--;
+        }
+        return true;
+    }
+};
