c03d0dd7a22c7911a9883691b08f0af21a01ca1f
Juhi <72224773+juhik24@users.noreply.github.com>
2022-08-30 01:24:38 +0530

    Time: 1224 ms (5.06%), Space: 780.8 MB (5.14%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -6,9 +6,7 @@ public:
         for(int i = 0; i < n; i++) {
             if(st.size() == 0 || st.top() != s[i]) st.push(s[i]);
             else {
-                while(st.size() > 0 && st.top() == s[i]) {
                 st.pop();
-            }
             }
         }
         string ans = "";
