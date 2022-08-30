4c4b35f50f72c0c1afa7e23fe91c42d98b96d67b
Juhi <72224773+juhik24@users.noreply.github.com>
2022-08-30 23:47:04 +0530

    Time: 8 ms (11.84%), Space: 8.3 MB (70.46%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -13,18 +13,15 @@ public:
         return partitionIndex;
     }
     
-    void QuickSort(vector<int>& nums, int start, int end) {
-        if(start < end) {
-            int partitionIndex = Partition(nums, start, end);
-            QuickSort(nums, start, partitionIndex-1);
-            QuickSort(nums, partitionIndex+1, end);
-        }
+    void QuickSort(vector<int>&nums, int start, int end) {
+        if(start > end) return;
+        int partitionIndex = Partition(nums, start, end);
+        QuickSort(nums, start, partitionIndex-1);
+        QuickSort(nums, partitionIndex+1, end);
     }
     
     void sortColors(vector<int>& nums) {
-         int n = nums.size();
-         int start = 0;
-         int end = n-1;
-         QuickSort(nums, start, end);
+        int n = nums.size();
+        QuickSort(nums, 0, n-1);
     }
 };
