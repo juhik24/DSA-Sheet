c2bdbbd55d63be27d0de268b9e817262e0b58835
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-01 00:46:02 +0530

    Time: 0 ms (100.00%), Space: 7 MB (29.79%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -7,25 +7,25 @@ public:
         int top = 0, down = n-1, left = 0, right = m-1;
         int dir = 0;
         while(top <= down && left <= right) {
-            if(dir == 0) { //left->right
+            if(dir == 0) {  // left to right
                 for(int i = left; i <= right; i++) {
                     ans.push_back(matrix[top][i]);
                 }
                 top++;
             }
-            else if(dir == 1) { //up->down
+            else if(dir == 1) {  // top to down
                 for(int i = top; i <= down; i++) {
                     ans.push_back(matrix[i][right]);
                 }
                 right--;
             }
-            else if(dir == 2) { //right->left
+            else if(dir == 2) {  // right to left
                 for(int i = right; i >= left; i--) {
                     ans.push_back(matrix[down][i]);
                 }
                 down--;
             }
-            else if(dir == 3) { //down->up
+            else if(dir == 3) {   // down to top
                 for(int i = down; i >= top; i--) {
                     ans.push_back(matrix[i][left]);
                 }
