6526c08a5293d725070cf82687a15da8b52dd4fa
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-03 02:10:50 +0530

    Time: 468 ms (6.07%), Space: 76.2 MB (83.33%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -3,40 +3,34 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         int lb[n], rb[n];
-        int max_area = INT_MIN;
         stack<int>st;
-        st.push(n-1);
         rb[n-1] = n;
+        st.push(n-1);
         for(int i = n-2; i >= 0; i--) {
-            while(st.size()>0 && heights[i] <= heights[st.top()]) {
+            while(st.size() > 0 && heights[st.top()] >= heights[i]) {
                 st.pop();
             }
-            if(st.size() == 0) {
-                rb[i] = n;
-            }
-            else {
-                rb[i] = st.top();
-            }
+            if(st.size() == 0) rb[i] = n;
+            else rb[i] = st.top();
             st.push(i);
         }
         while(!st.empty()) st.pop();
         lb[0] = -1;
         st.push(0);
         for(int i = 1; i < n; i++) {
-            while(st.size() > 0 && heights[i] <= heights[st.top()]) {
+            while(st.size() > 0 && heights[st.top()] >= heights[i]) {
                 st.pop();
             }
-            if(st.size() == 0) {
-                lb[i] = -1;
-            }
+            if(st.size() == 0) lb[i] = -1;
             else {
                 lb[i] = st.top();
             }
             st.push(i);
         }
+        int max_area = INT_MIN;
         for(int i = 0; i < n; i++) {
-            int width = rb[i]-lb[i]-1;
-            int area = heights[i]*width;
+            int width = rb[i] - lb[i] - 1;
+            int area = width * heights[i];
             if(area > max_area) {
                 max_area = area;
             }
