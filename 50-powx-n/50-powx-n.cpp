59ad3c0cc7b6c4c15e6e747e5598a2022cde87bf
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-04 01:47:32 +0530

    Time: 4 ms (34.01%), Space: 5.9 MB (46.07%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long nn = n;
+        if(nn < 0) nn = -1 * nn;
+        while(nn) {
+            if(nn%2 == 0) {
+                x = x*x;
+                nn = nn/2;
+            }
+            else {
+                ans = ans * x;
+                nn = nn - 1;
+            }
+        }
+        if(n < 0) {
+            ans = 1/ans;
+        }
+        return ans;
+    }
+};
+
+
+// Brute force = time = O(n),  space = O(1) 
+
+// double myPow(double x, int n) {
+//         double ans = x;
+//         if(n == 0) return 1.00000;
+//         if(n > 0) {
+//             n=n-1;
+//             while(n--) {
+//                 ans *= x;
+//             }
+//         }
+//         else {
+//             n = n+1;
+//             while(n < 0) {
+//                 ans *= x;
+//                 n++;
+//             }
+//             ans = 1/ans;
+//         }
+//         return ans;
+//     }
