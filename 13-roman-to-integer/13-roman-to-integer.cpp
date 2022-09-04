8068c8a9c2a88f7556f92c4e70fe166f8611a3b5
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-05 00:06:49 +0530

    Time: 30 ms (25.98%), Space: 8 MB (44.40%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
+    // Time = O(n), Space = O(1) 
+    
     int romanToInt(string s) {
-        unordered_map<char, int>mp = {{'I', 1},
-                                      {'V', 5},
-                                      {'X', 10},
-                                      {'L', 50},
-                                      {'C', 100},
-                                      {'D', 500},
-                                      {'M', 1000}};
-        int ans = 0;
-        for(int i = 0; i < s.length()-1; i++) {
-            if(mp[s[i]] < mp[s[i+1]]) {
-                ans = ans - mp[s[i]];
+        unordered_map<char, int>mp;
+        mp.insert({'I', 1});
+        mp.insert({'V', 5});
+        mp.insert({'X', 10});
+        mp.insert({'L', 50});
+        mp.insert({'C', 100});
+        mp.insert({'D', 500});
+        mp.insert({'M', 1000});
+        int n = s.length();
+        int ans = mp[s[n-1]];
+        for(int i = n-2; i >= 0; i--) {
+            if(mp[s[i]] < mp[s[i+1]]) {  // if s[i] is smaller than s[i+1] then subtract the value of s[i] to result
+                ans -= mp[s[i]];
             }
             else {
-                ans += mp[s[i]];
+                ans += mp[s[i]];   // if s[i] is greater than s[i+1] then add the value of s[i] to result
             }
         }
-        ans += mp[s[s.length()-1]];
         return ans;
     }
 };
