82c2dfc9692e8f9968094bbf7466d63fd6353eb0
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-04 16:23:34 +0530

    Time: 21 ms (29.44%), Space: 7.4 MB (8.78%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
+    
+    // Time = O(nlogn), Space = O(1) 
+    
     bool isAnagram(string s, string t) {
-        unordered_map<char, int>mp;
-        int n = s.length();
-        int m = t.length();
-        if(n != m) return false;
-        for(int i = 0; i < n; i++) {
-            mp[s[i]]++;
-        }
-        for(int i = 0; i < m; i++) {
-            if(mp.find(t[i]) == mp.end()) return false;
-            else {
-                mp[t[i]]--;
-                if(mp[t[i]] == 0) mp.erase(t[i]);
-            }
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s == t) return true;
+        return false;
     }
 };
+
+
+// Brute force = Time = O(n), Space = O(n) 
+
+// bool isAnagram(string s, string t) {
+//         unordered_map<char, int>mp;
+//         int n = s.length();
+//         int m = t.length();
+//         if(n != m) return false;
+//         for(int i = 0; i < n; i++) {
+//             mp[s[i]]++;
+//         }
+//         for(int i = 0; i < m; i++) {
+//             if(mp.find(t[i]) == mp.end()) return false;
+//             else {
+//                 mp[t[i]]--;
+//                 if(mp[t[i]] == 0) mp.erase(t[i]);
+//             }
+//         }
+//         return true;
+//     }
