ddb2af0be2e5cc7d45af24aa79a6c6580a8f2b5b
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-04 23:50:37 +0530

    Time: 7 ms (85.57%), Space: 6 MB (78.86%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // Time = O(number of characters in the resulting string),  Space = O(1) 
+    
     string intToRoman(int num) {
         int arr[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         string symbol[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
