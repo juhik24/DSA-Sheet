d1b32d02dbc2028113061c5a385029b1c49502d0
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-05 02:16:49 +0530

    Time: 0 ms (100.00%), Space: 13.7 MB (47.48%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    void solve(int open, int close, string &op, vector<string>&v) {
+    //Backtracking method - in this we'll add open parenthesis if open count < n
+    // and we'll add a closing parenthesis if open count > closed
+    // and we'll stop adding parenthesis when open == close == n
+    
+    void solve(int open, int close, vector<string>&ans, string &op) {
         if(open == 0 && close == 0) {
-            v.push_back(op);
+            ans.push_back(op);
             return;
         }
         if(open != 0) {
             string op1 = op;
             op1.push_back('(');
-            solve(open-1, close, op1, v);
+            solve(open-1, close, ans, op1);
         }
         if(close > open) {
             string op2 = op;
             op2.push_back(')');
-            solve(open, close-1, op2, v);
+            solve(open, close-1, ans, op2);
         }
     }
     
     vector<string> generateParenthesis(int n) {
-        vector<string>v;
+        vector<string>ans;
         int open = n, close = n;
         string op = "";
-        solve(open, close, op, v);
-        return v;
+        solve(open, close, ans, op);
+        return ans;
     }
 };
