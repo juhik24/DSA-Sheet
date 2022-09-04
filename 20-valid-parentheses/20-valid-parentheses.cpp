5f2e0fc79f839f6e4561e0d5232e3674111d83d1
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-04 16:08:41 +0530

    Time: 8 ms (5.48%), Space: 6.4 MB (10.05%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
+    
+    // Time = O(n), Space = O(n) 
+    
     bool isValid(string s) {
         int n = s.length();
         stack<char>st;
-        if(n == 1) return false;
         for(int i = 0; i < n; i++) {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
                 st.push(s[i]);
             }
-            else if(s[i] == ')' && st.size() > 0 && st.top() == '(') {
-                st.pop();
-            }
-            else if(s[i] == '}' && st.size() > 0 && st.top() == '{') {
-                st.pop();
-            }
-            else if(st.size() > 0 && s[i] == ']' && st.top() == '[') {
-                st.pop();
-            }
+            else if(st.size() > 0 && st.top() == '(' && s[i] == ')') st.pop();
+            else if(st.size() > 0 && st.top() == '{' && s[i] == '}') st.pop();
+            else if(st.size() > 0 && st.top() == '[' && s[i] == ']') st.pop();
             else return false;
         }
         if(st.size() == 0) return true;
