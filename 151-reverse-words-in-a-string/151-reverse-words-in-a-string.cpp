057bb0d1d53cf3fedb77ae054e071c098ed1fb5c
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-06 17:31:18 +0530

    Time: 3 ms (94.14%), Space: 7.1 MB (91.05%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,27 +1,54 @@
 class Solution {
 public:
+    // Time = O(n), Space = O(1) 
+    
     string reverseWords(string s) {
         int n = s.length();
-        string ans = "";
-        stack<string>st;
-        for(int i = 0; i < n; i++) {
-            if(s[i] == ' ') continue;
-            string temp = "";
+        reverse(s.begin(), s.end());
+        int i = 0, j = 0, k = 0;
+        while(i < n) {
             while(i < n && s[i] != ' ') {
-                temp += s[i];
-                i++;
+                s[j] = s[i];
+                j++; i++;
             }
-            st.push(temp);
+            if(k < j) {
+                reverse(s.begin()+k, s.begin()+j);
+                if(j == n) break;
+                s[j] = ' ';
+                j++;
+                k = j;
+            }
+            i++;
         }
-        while(st.size() > 0) {
-            if(ans == "") {
-                ans += st.top();
-            }
-            else {
-                ans += " " + st.top();
-            }
-            st.pop();
-        }
-        return ans;
+        if(j > 0 && s[j-1] == ' ') j--;
+        s.resize(j);
+        return s;
     }
 };
+
+// Brute force = Time = O(n) , Space = O(n) 
+
+// string reverseWords(string s) {
+//         int n = s.length();
+//         string ans = "";
+//         stack<string>st;
+//         for(int i = 0; i < n; i++) {
+//             if(s[i] == ' ') continue;
+//             string temp = "";
+//             while(i < n && s[i] != ' ') {
+//                 temp += s[i];
+//                 i++;
+//             }
+//             st.push(temp);
+//         }
+//         while(st.size() > 0) {
+//             if(ans == "") {
+//                 ans += st.top();
+//             }
+//             else {
+//                 ans += " " + st.top();
+//             }
+//             st.pop();
+//         }
+//         return ans;
+//     }
