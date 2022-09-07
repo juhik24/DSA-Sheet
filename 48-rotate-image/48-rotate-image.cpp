da74a6b5d03b18b18e0229dabf9c6faa69eb726a
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-08 00:32:29 +0530

    Time: 3 ms (63.65%), Space: 7.2 MB (33.37%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
+    // Time = O(n^2), Space = O(1) 
+    // In this we'll transpose the matrix and then reverse each row
+    // In transpose columns will become rows
+    
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int ans[n][n];
-        memset(ans, 0, sizeof(ans));
-        int col = n-1;
+        //Transpose
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                ans[j][col] = matrix[i][j];
+            for(int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
-            col--;
         }
+        //Reverse
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                matrix[i][j] = ans[i][j];
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
+
+// Brute force = Time = O(n^2), Space = O(n^2) 
+
+// void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         int ans[n][n];
+//         memset(ans, 0, sizeof(ans));
+//         int col = n-1;
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 ans[j][col] = matrix[i][j];
+//             }
+//             col--;
+//         }
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 matrix[i][j] = ans[i][j];
+//             }
+//         }
+//     }
