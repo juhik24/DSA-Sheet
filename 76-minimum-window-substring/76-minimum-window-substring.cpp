fcf961a0a095ce0e547ac909654c794a440045c8
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-07 12:08:35 +0530

    Time: 41 ms (39.76%), Space: 7.7 MB (68.91%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {  
-        int n = s.length();
-        int m = t.length();
-        unordered_map<char, int>mp;
-        for(int i = 0; i < m; i++) {
-            mp[t[i]]++;
+    // Time = O(n), Space = O(n) 
+    
+    string minWindow(string s, string t) {
+        string ans = "";
+        unordered_map<char, int>mp2;
+        for(int i = 0; i < t.length(); i++) {
+            mp2[t[i]]++;
         }
-        int res = INT_MAX;
-        int cnt = mp.size();
-        int i = 0, j = 0, start;
-        while(j < n) {
-            mp[s[j]]--;
-            if(mp[s[j]] == 0) cnt--;
-            if(cnt == 0) {
-                while(cnt == 0) {
-                    if(j-i+1 < res) {
-                        res = j-i+1;
-                        start = i;
-                    }
-                    mp[s[i]]++;
-                    if(mp[s[i]] == 1) cnt++;
-                    i++;
-                }
+        int mcnt=0;
+        int start = 0;
+        int dmcnt = t.length();
+        int length = INT_MAX;
+        unordered_map<char, int>mp1;
+        int i = 0, j = 0;
+        while(i < s.length()) {
+            mp1[s[i]]++;
+            if(mp1[s[i]] <= mp2[s[i]]) {
+                mcnt++;
             }
-            j++;
+            while(mcnt == dmcnt) {
+                if(length > i-j+1) {
+                    start = j;
+                    length = i-j+1;
+                }
+                mp1[s[j]]--;
+                if(mp1[s[j]] < mp2[s[j]]) {
+                    mcnt--;
+                }
+                j++;
+            }
+            i++;
         }
-        if(res != INT_MAX) return s.substr(start, res);
-        else return "";
+        if(length == INT_MAX) return "";
+        return s.substr(start, length);
     }
 };
