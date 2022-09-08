5fe293c13f1a702281180a7f22072eb7d85b736b
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-09 03:16:14 +0530

    Time: 8 ms (10.21%), Space: 5.9 MB (27.68%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n == 0) return false;
-        if(n == 1 || n == 2) return true;
-        if(n%2 == 0) {
-            while(n % 2 == 0) {
-                n = n/2;
-            }
-            if(n == 1) return true;
-            else return false;
-        }
+        if(n <= 0) return false;
+        long long val = (n & (n-1));
+        if(val == 0) return true;
         else return false;
     }
 };
+
+// Brute force = O(log n base 2), Space = O(1) 
+
+// bool isPowerOfTwo(int n) {
+//         if(n == 0) return false;
+//         if(n == 1 || n == 2) return true;
+//         if(n%2 == 0) {
+//             while(n % 2 == 0) {
+//                 n = n/2;
+//             }
+//             if(n == 1) return true;
+//             else return false;
+//         }
+//         else return false;
+//     }
