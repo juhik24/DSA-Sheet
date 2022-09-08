a3d52c043fbb10db6036d46d4c0632a17c070c8c
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-09 00:38:01 +0530

    Time: 55 ms (83.01%), Space: 27.7 MB (79.53%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -2,32 +2,9 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         int n = nums.size();
-        int max1= INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
-        int min1 = INT_MAX, min2 = INT_MAX;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = nums[i];
-            }
-            else if(nums[i] > max2) {
-                max3 = max2;
-                max2 = nums[i];
-            }
-            else if(nums[i] > max3) {
-                max3 = nums[i];
-            }
-            if(nums[i] < min1) {
-                min2 = min1;
-                min1 = nums[i];
-            }
-            else if(nums[i] < min2) {
-                min2 = nums[i];
-            }
-        }
-        int a = max1*max2*max3;
-        int b = min1*min2*max1;
-        int ans = max(a, b);
-        return ans;
+        sort(nums.begin(), nums.end());
+        int a = nums[n-1]*nums[n-2]*nums[n-3];
+        int b = nums[0]*nums[1]*nums[n-1];
+        return max(a, b);
     }
 };
