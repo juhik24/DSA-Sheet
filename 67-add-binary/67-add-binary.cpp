2581ae4ea876508e19e893511d7a30176a45e81f
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-09 00:33:48 +0530

    Time: 9 ms (19.03%), Space: 6.9 MB (30.69%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
+    // Time = O(max length of(a, b)), Space = O(1) 
+    
     string addBinary(string a, string b) {
-        int n = a.length();
-        int m = b.length();
+        int n = a.length(), m = b.length();
         int carry = 0;
         string ans = "";
         int i = n-1, j = m-1;
         while(i >= 0 || j >= 0 || carry != 0) {
             int x = 0, y = 0;
-            if(i >= 0 && a[i] == '1') {
-                x = 1;
-            }
-            if(j >= 0  && b[j] == '1') y = 1;
-            int sum = (x+y+carry)%2;
+            if(i >= 0 && a[i] == '1') x = 1;
+            if(j >= 0 && b[j] == '1') y = 1;
+            int sum = (x+y+carry) % 2;
             ans = to_string(sum) + ans;
-            carry = (x+y+carry)/2;   
+            carry = (x+y+carry)/2;
             if(i >= 0) i--;
             if(j >= 0) j--;
         }
