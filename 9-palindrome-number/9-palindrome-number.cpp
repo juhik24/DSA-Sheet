8ac0dee90ae521e3126facab0a71d40aad74d891
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-09 02:14:00 +0530

    Time: 41 ms (11.24%), Space: 5.9 MB (74.25%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
+    // Time = O(log n), Space = O(1) 
+    
     bool isPalindrome(int x) {
-        string str = to_string(x);
-        int n = str.length();
-        int i = 0, j = n-1;
-        while(i < j) {
-            if(str[i] == str[j]) {
-                i++;
-                j--;
-            }
-            else return false;
+        //when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        if(x < 0 || (x%10 == 0 && x != 0)) return false;
+        
+        int reverse = 0;
+        while(reverse < x) {
+            reverse = (reverse*10) + x%10;
+            x = x/10;
         }
-        return true;
+         // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+        if(x == reverse || reverse/10 == x) return true;
+        return false;
     }
 };
