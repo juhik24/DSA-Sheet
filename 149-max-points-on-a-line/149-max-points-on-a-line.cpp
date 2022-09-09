6991f716b575f745de6f664134f7d8c0b71a2ee8
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-09 11:51:54 +0530

    Time: 41 ms (79.33%), Space: 11.4 MB (76.12%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    // Time = O(n^2). Space = O(n) 
+    
+    // slope = y2 - y1 / x2 - x1
+    // ---- horizontal and | vertical
+    
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
         if(n == 1 || n == 2) return n;
         unordered_map<double, int>mp;
         float slope;
         int max = 0;
+        // we'll take two points and calculate the slope, if slope is same then it lies in the same line.
         for(int i = 0; i < n; i++) {
             int imax=0;
             for(int j = i+1; j < n; j++) {
+                // if points lie on the same horizontal line then there is no slope.
                 if(points[j][0]-points[i][0] == 0) slope = INT_MAX;
                 else {
                     slope = (float)(points[j][1]-points[i][1])/(float)(points[j][0]-points[i][0]);
