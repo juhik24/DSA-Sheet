2cc399749a6711f121cb151bef15496599266ae5
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-12 01:52:42 +0530

    Time: 98 ms (15.78%), Space: 17.8 MB (77.50%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -10,6 +10,8 @@
  */
 class Solution {
 public:
+    // Time = O(n), Space = O(1) 
+    
     ListNode* reverse(ListNode* head) {
         ListNode* cur = head, *prev = NULL, *next;
         while(cur) {
