f0fef4c82752c886e3445420ccb2ad86d2840242
Juhi <72224773+juhik24@users.noreply.github.com>
2022-09-14 00:09:07 +0530

    Time: 88 ms (15.21%), Space: 89.6 MB (44.67%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> merge(vector<int>& nums1, vector<int> &nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<int>v(m+n);
+        int i = 0, j = 0, k = 0;
+        while(i < n && j < m) {
+            if(nums1[i] <= nums2[j]) {
+                v[k] = nums1[i];
+                i++;
+            }
+            else {
+                v[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < n) {
+            v[k] = nums1[i];
+            k++; i++;
+        }
+        while(j < m) {
+            v[k] = nums2[j];
+            k++; j++;
+        }
+        return v;
+    }
+    
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> v = merge(nums1, nums2);
+        int n = v.size();
+        if(n%2 == 0) {
+            int m1 = n/2;
+            int m2 = m1-1;
+           // cout << m1 << " " << m2;
+            int med = (v[m1]+v[m2])*1.0/2.0;
+            cout << med;
+            if((v[m1]+v[m2])%2 > 0) {
+                return med+0.5;
+            }
+            else return med;
+        }
+        else  {
+            int m = n/2;
+            cout << m;
+            return v[m]*1.0;
+        }
+    }
+};
