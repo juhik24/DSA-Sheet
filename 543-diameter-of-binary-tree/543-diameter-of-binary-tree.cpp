e546c7cafe4d506f5b9903d4f74da2286afa3a4f
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-03 00:10:27 +0530

    Time: 27 ms (22.41%), Space: 20.2 MB (64.21%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,22 +11,16 @@
  */
 class Solution {
 public:
-    // For any given node, we'll find out the height of left half and right half and then we'll add them up.
-    // so that is the longest path through the given node.
-    // So if we find left height + right height across every given node, so max of left height and right height will be the diameter of the tree.
+    // Time = O(n), Space = O(n)->recursion stack
     
-    int Height(TreeNode* root) {
+    // We can optimize the brute force by calculating height in the same recursion rather than calling a height function separately for every node.
+    
+    int solve(TreeNode* root, int &d) {
         if(root == NULL) return 0;
-        return max(Height(root->left), Height(root->right))+1;
-    }
-    
-    void solve(TreeNode* root, int &d) {
-        if(root == NULL) return;
-        int left = Height(root->left);
-        int right = Height(root->right);
-        d = max(d, left+right);
-        solve(root->left, d);
-        solve(root->right, d);
+        int left = solve(root->left, d);
+        int right = solve(root->right, d);
+        d = max(d, left + right);
+        return max(left, right)+1;
     }
     
     int diameterOfBinaryTree(TreeNode* root) {
@@ -35,3 +29,30 @@ public:
         return d;
     }
 };
+
+// Brute force: Time = O(n^2) as we'll calculate height for every node, Space = O(n)->for recursion stack
+
+// For any given node, we'll find out the height of left half and right half and then we'll add them up.
+    // so that is the longest path through the given node.
+    // So if we find left height + right height across every given node, so max of left height and right height will be the diameter of the tree.
+    // We have to find the left height & right height for every node.
+    
+//     int Height(TreeNode* root) {
+//         if(root == NULL) return 0;
+//         return max(Height(root->left), Height(root->right))+1;
+//     }
+    
+//     void solve(TreeNode* root, int &d) {
+//         if(root == NULL) return;
+//         int left = Height(root->left);
+//         int right = Height(root->right);
+//         d = max(d, left+right);
+//         solve(root->left, d);
+//         solve(root->right, d);
+//     }
+    
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         int d = 0;
+//         solve(root, d);
+//         return d;
+//     }
