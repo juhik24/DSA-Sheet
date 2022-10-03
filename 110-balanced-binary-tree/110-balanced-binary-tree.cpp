b6b6c1750438f835bbabb094e580b50d0c9834a7
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-03 17:09:44 +0530

    Time: 22 ms (56.29%), Space: 20.8 MB (94.71%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,18 +11,26 @@
  */
 class Solution {
 public:
-    bool ans = true;
-    int height(TreeNode* root) {
+    // Time = O(n), Space = O(n)->recursion stack
+    
+    // In optimized approach, we'll calculate the height in the same dfs recursion
+    int height(TreeNode* root, bool &ans) {
         if(root == NULL) return 0;
-        int left = height(root->left);
-        int right = height(root->right);
+        int left = height(root->left, ans);
+        int right = height(root->right, ans);
+        if(ans == false) return false;
         if(abs(left-right) > 1) ans = false;
         return max(left, right)+1;
     }
     
     bool isBalanced(TreeNode* root) {
+        bool ans = true;
         if(root == NULL) return true;
-        height(root);
+        height(root, ans);
         return ans;
     }
 };
+
+// Brute force = O(n^2), Space = O(n)->recursion stack
+
+// Here we'll calculate height of left subtree and right subtree for every node and check if its difference is less than or equal to 1. 
