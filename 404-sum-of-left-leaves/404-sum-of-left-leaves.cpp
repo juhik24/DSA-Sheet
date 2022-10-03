4f35c12fc5af4eaee4bdcd79a7977ec0d66421c3
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-03 16:50:09 +0530

    Time: 8 ms (44.15%), Space: 13.3 MB (97.00%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -13,6 +13,9 @@ class Solution {
 public:
     // Time = O(n), Space = O(n) 
     
+    // Here we'll maintain a bool flag which will be true for left side and false for right side.
+    // and traverse the tree in preorder and check if it is a leaf node (that is left child is NULL & right child is NULL) and flag is true then it is left leaf and we'll add its value to the sum
+    
     void solve(TreeNode* root, int &sum, bool flag) {
         if(root == NULL) return;
         if(root->left == NULL && root->right == NULL) {
