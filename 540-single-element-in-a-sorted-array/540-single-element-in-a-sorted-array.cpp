63780de406c7cfe163da40aeae4aac8d766dd5ab
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-05 19:54:09 +0530

    Time: 54 ms (41.37%), Space: 22.4 MB (55.50%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    // Time = O(log n), Space = O(1) 
+    
+    // Binary Search
+    // in order to check for left half of single element
+    // 1st instance = even index
+    // 2nd instance = odd index
+    
+// for right half - 1st instance = odd index & 2nd instance = even index
+    
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         int left = 0, right = n-1;
