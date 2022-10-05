22ab2d7fc3b342f14e5683284a73e91e04dd5014
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-06 02:00:06 +0530

    Time: 32 ms (70.84%), Space: 16.2 MB (59.08%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -11,26 +11,52 @@
  */
 class Solution {
 public:
-    void innerDFS(TreeNode* root, int targetSum, long long sum, int &cnt) {
-        if(root == NULL) return;
-        sum += root->val;
-        if(sum == targetSum) cnt++;
-        innerDFS(root->left, targetSum, sum, cnt);
-        innerDFS(root->right, targetSum, sum, cnt);
-    }
+    // Time = O(n), Space = O(n)->vector
     
-    void outerDFS(TreeNode* root, int targetSum, int &cnt) {
+    void solve(TreeNode* root, int targetSum, vector<int>&path, int &cnt) {
         if(root == NULL) return;
+        path.push_back(root->val);
+        solve(root->left, targetSum, path, cnt);
+        solve(root->right, targetSum, path, cnt);
         long long sum = 0;
-        innerDFS(root, targetSum, sum, cnt);
-        outerDFS(root->left, targetSum, cnt);
-        outerDFS(root->right, targetSum, cnt);
+        for(int i = path.size()-1; i >= 0; i--) {
+            sum += path[i];
+            if(sum == targetSum) cnt++;
+        }
+        path.pop_back();
     }
     
     int pathSum(TreeNode* root, int targetSum) {
         int cnt = 0;
-        if(root == NULL) return cnt;
-        outerDFS(root, targetSum, cnt);
+        vector<int>paths;
+        solve(root, targetSum, paths, cnt);
         return cnt;
     }
 };
+
+// Brute force = Time = O(n^2), Space = O(n)->recursion stack
+
+// Here we'll be using an outer DFS for selecting each node and an inner DFS for calculating the sum and checking if we'll get our target Sum from that node.
+
+// void innerDFS(TreeNode* root, int targetSum, long long sum, int &cnt) {
+//         if(root == NULL) return;
+//         sum += root->val;
+//         if(sum == targetSum) cnt++;
+//         innerDFS(root->left, targetSum, sum, cnt);
+//         innerDFS(root->right, targetSum, sum, cnt);
+//     }
+    
+//     void outerDFS(TreeNode* root, int targetSum, int &cnt) {
+//         if(root == NULL) return;
+//         long long sum = 0;
+//         innerDFS(root, targetSum, sum, cnt);
+//         outerDFS(root->left, targetSum, cnt);
+//         outerDFS(root->right, targetSum, cnt);
+//     }
+    
+//     int pathSum(TreeNode* root, int targetSum) {
+//         int cnt = 0;
+//         if(root == NULL) return cnt;
+//         outerDFS(root, targetSum, cnt);
+//         return cnt;
+//     }
