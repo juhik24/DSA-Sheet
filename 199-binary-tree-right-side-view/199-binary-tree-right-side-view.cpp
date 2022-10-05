e5a004cd89ee5478895d41117dd323c08ad61c5e
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-05 16:19:19 +0530

    Time: 3 ms (86.29%), Space: 12 MB (70.74%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,20 +11,29 @@
  */
 class Solution {
 public:
-    vector<int>ans;
+    // Time = O(n), Space = O(h)
     
-    void solve(TreeNode* root, int level) {
+    // In this we'll do reverse preorder (preorder = Root L R, reverse preorder = Root R L)
+    // We'll maintain a level variable, if level == ans.size() then we'll push that in our ans vector.
+    // As we are first going in right side so the first element on that level will be rightmost element.
+    
+    void solve(TreeNode* root, int level, vector<int> &ans) {
         if(root == NULL) return;
         if(level == ans.size()) {
             ans.push_back(root->val);
         }
-        solve(root->right, level+1);
-        solve(root->left, level +1);
+        solve(root->right, level+1, ans);
+        solve(root->left, level +1, ans);
     }
     
     vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
         if(root == NULL) return ans;
-        solve(root, 0);
+        solve(root, 0, ans);
         return ans;
     }
 };
+
+//Brute force: Time = O(n), Space = O(n)->Worst case(skew tree)
+
+// We'll do level order traversal in this.
