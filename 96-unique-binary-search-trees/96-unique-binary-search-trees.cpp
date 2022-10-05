7e3baa6c9b57c146935fa15d9780e6aed5f3e03b
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-06 02:24:27 +0530

    Time: 3 ms (37.39%), Space: 6 MB (70.74%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    // Time = O(n^2), Space = O(n)
+    
+    // Solved using nth catalan number ( 1 1 2 5 14)
+    // C0 = 1, C1 = 0
+    // C2 = (C0*C1) + (C1 * C0)
+    // C3 = (C0*C2) + (C1*C1) + (C2*C0), C4 = (C0*C3) + (C1*C2) + (C2*C1) + (C3*C0)
+    // and so on
+    
     int numTrees(int n) {
         int dp[n+1];
         dp[0] = 1;
