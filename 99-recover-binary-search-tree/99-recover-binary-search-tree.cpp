f0ea4ae3170ed436853818473fd72d81064e6d81
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-06 13:26:55 +0530

    Time: 60 ms (49.38%), Space: 57.9 MB (45.13%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,28 +11,60 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>&v) {
-        if(root == NULL) return;
-        inorder(root->left, v);
-        v.push_back(root->val);
-        inorder(root->right, v);
-    }
+    // Time = O(n), Space = O(n)->recursion stack
     
-    void solve(TreeNode* root, vector<int>&v, int &i) {
+    // Here also we'll do inorder traversal and keep a prev pointer if value of prev < curr node then we'll prev and curr node in first and second pointer and if again got prev < curr then we'll store curr in last pointer. And we'll swap (first, last). If we didn't get last then we'll swap(first , middle).
+    
+    TreeNode* prev, *first, *middle, *last;
+    
+    void inorder(TreeNode* root) {
         if(root == NULL) return;
-        solve(root->left, v, i);
-        if(root->val != v[i]) {
-            root->val = v[i];
+        inorder(root->left);
+        if(root->val < prev->val) {
+            if(first == NULL) {
+                first = prev;
+                middle = root;
+            }
+            else last = root;
         }
-        i++;
-        solve(root->right, v, i);
+        prev = root;
+        inorder(root->right);
     }
     
     void recoverTree(TreeNode* root) {
-        vector<int>v;
-        inorder(root, v);
-        sort(v.begin(), v.end());
-        int i = 0;
-        solve(root, v, i);
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last) swap(first->val, last->val);
+        else swap(first->val, middle->val);
     }
 };
+
+// Brute force = Time = O(n+nlogn), Space = O(n)->vector
+
+// Here we'll store all node values in a vector and then sort it. as inorder of bst contains elements in sorted order. And then we can assign vector values to nodes . 
+
+// void inorder(TreeNode* root, vector<int>&v) {
+//         if(root == NULL) return;
+//         inorder(root->left, v);
+//         v.push_back(root->val);
+//         inorder(root->right, v);
+//     }
+    
+//     void solve(TreeNode* root, vector<int>&v, int &i) {
+//         if(root == NULL) return;
+//         solve(root->left, v, i);
+//         if(root->val != v[i]) {
+//             root->val = v[i];
+//         }
+//         i++;
+//         solve(root->right, v, i);
+//     }
+    
+//     void recoverTree(TreeNode* root) {
+//         vector<int>v;
+//         inorder(root, v);
+//         sort(v.begin(), v.end());
+//         int i = 0;
+//         solve(root, v, i);
+//     }
