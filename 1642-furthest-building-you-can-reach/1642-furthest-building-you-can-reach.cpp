bddc8ade9c87d7ad29ce9fe4bff12d5503ef092a
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-09 19:03:27 +0530

    Time: 287 ms (19.13%), Space: 53.8 MB (59.02%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
+    // Time = O(nlogL)-> L = ladders, Space = O(L)
+    
+    // We'll create a min priority queue and maintain a variable bricksUsed.
+    // Whenever we see the next building is greater(i.e, the diff of cuurent building & next building is greater than 0 then we'll store the diff in priority queue)
+    // If pq.size() becomes greater than no. of ladders then we'll pop the top element from pq (i.e, min diff) and add it to bricksUsed.
+    // If bricksUsed becomes greater than no. of bricks then we'll return that index, because that will be the last index.
+    
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         int n = heights.size();
-        priority_queue<int, vector<int>, greater<int>>pq;
+        priority_queue<int, vector<int>, greater<int>>pq;  // min priority queue
         int bricksUsed = 0;
         for(int i = 1; i < n; i++) {
             int diff = heights[i] - heights[i-1];
