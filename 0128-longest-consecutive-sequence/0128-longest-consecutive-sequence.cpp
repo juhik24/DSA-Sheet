87f8b6153ea65f0dd4057988ca4af9da06b8d8d8
Juhi <72224773+juhik24@users.noreply.github.com>
2022-10-25 22:59:35 +0530

    Time: 271 ms (65.08%), Space: 49.8 MB (56.85%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,24 +1,52 @@
 class Solution {
 public:
-    // Time = O(nlogn), Space = O(1) 
+    // Time = O(n), Space = O(n) 
     
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
-        if(n == 0) return 0;
-        sort(nums.begin(), nums.end());
-        int len = 1, maxLen = 1;
-        int ele = nums[0];
-        for(int i = 1; i < n; i++) {
-            if(ele+1 == nums[i]) {
-                ele = ele+1;
+        unordered_set<int>s;
+        for(int i = 0; i < n; i++) {
+            s.insert(nums[i]);
+        }
+        int maxLen = 0;
+        for(auto num : s) {
+            int len = 1;
+           // find consecutive elements in the backward and forward direction from num
+            int j = 1, k = 1;
+            while(s.find(num-j) != s.end()) {
+                s.erase(num-j);
                 len++;
+                j++;
             }
-            else if(ele+1 < nums[i]) {
-                ele = nums[i];
-                len = 1;
+            while(s.find(num+k) != s.end()) {
+                s.erase(num+k);
+                len++;
+                k++;
             }
             maxLen = max(len, maxLen);
         }
         return maxLen;
     }
 };
+
+// Time = O(nlogn), Space = O(1) 
+    
+    // int longestConsecutive(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if(n == 0) return 0;
+    //     sort(nums.begin(), nums.end());
+    //     int len = 1, maxLen = 1;
+    //     int ele = nums[0];
+    //     for(int i = 1; i < n; i++) {
+    //         if(ele+1 == nums[i]) {
+    //             ele = ele+1;
+    //             len++;
+    //         }
+    //         else if(ele+1 < nums[i]) {
+    //             ele = nums[i];
+    //             len = 1;
+    //         }
+    //         maxLen = max(len, maxLen);
+    //     }
+    //     return maxLen;
+    // }
